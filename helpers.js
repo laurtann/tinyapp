c939faee2fbf07266d29d80b011ed4d6c3eb50e2
Laura Tannahill <lntannahill@gmail.com>
2020-11-12 21:08:42 +0000

    changed minor logic issue in fetchUserByEmail

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@
 const fetchUserFromEmail = function(database, email) {
   for (let user in database) {
     if (database[user].email === email) {
-      return user;
+      return database[user];
     }
   } return false;
 };
