6de0d2b972cb0a0ba8050a664d86c3a7133a5711
Laura Tannahill <lntannahill@gmail.com>
2020-11-12 21:35:07 +0000

    Created mocha/chai testing to test helper function

---
+++ test/helpersTest.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+
+const { fetchUserFromEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('fetchUserFromEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = fetchUserFromEmail(testUsers, "user@example.com");
+    const expectedOutput = "userRandomID";
+    assert.isTrue(user.id === expectedOutput);
+  });
+
+  it("should return false if email is not in database", function() {
+    const user = fetchUserFromEmail(testUsers, "tinyapp@example.com");
+    const expectedOutput = false;
+    assert.isTrue(user === expectedOutput);
+  });
+});
